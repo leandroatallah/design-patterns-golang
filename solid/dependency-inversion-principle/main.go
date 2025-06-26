a30ef7ca332143123a4ea706ffdc1b70763b9b80
Leandro Atallah <leandro.atallah@vodafone.com>
2025-06-26 17:06:29 +0100

    Add example and notes to Dependency Inversion Principle

---
+++ solid/dependency-inversion-principle/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Abstraction
+type EmployeeLister interface {
+	GetName() string
+	GetSkills() []string
+}
+
+type Skill struct {
+	name string
+}
+
+type Developer struct {
+	name   string
+	skills []Skill
+}
+
+// Developer implements EmployeeList
+func (d *Developer) GetName() string {
+	return d.name
+}
+func (d *Developer) GetSkills() []string {
+	result := []string{}
+	for _, s := range d.skills {
+		result = append(result, s.name)
+	}
+	return result
+}
+func (d *Developer) AddSkill(skill *Skill) {
+	d.skills = append(d.skills, *skill)
+}
+
+type Team struct {
+	// Now, Team depends on EmployeeLister abstraction
+	employees []EmployeeLister
+}
+
+func (e *Team) ListEmployees() {
+	// It iterates over abstraction and not concrete type
+	for _, e := range e.employees {
+		fmt.Printf("Name: %s\n", e.GetName())
+		fmt.Printf("Skills: %s\n\n", strings.Join(e.GetSkills(), ", "))
+	}
+}
+
+func main() {
+	dev1 := Developer{"Phill Brooks", []Skill{}}
+	dev2 := Developer{"Colt Cabana", []Skill{}}
+
+	skl1 := Skill{"React"}
+	skl2 := Skill{"Golang"}
+
+	dev1.AddSkill(&skl1)
+	dev1.AddSkill(&skl2)
+	dev2.AddSkill(&skl2)
+
+	// It injects abstract types
+	team1 := Team{[]EmployeeLister{&dev1, &dev2}}
+	team1.ListEmployees()
+}
