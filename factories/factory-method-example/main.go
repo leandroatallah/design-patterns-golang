eb526c61b68a60bb5007b6da979a75bd99725d92
Leandro Atallah <leandro.atallah@vodafone.com>
2025-07-02 11:08:30 +0100

    Add factory method example

---
+++ factories/factory-method-example/main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+/*
+* Employee (Product) interface: Define the common interface for the objects the factory method creates.
+* EmployeeCreator (Abstract Creator): Declares the factory method, which concreate creators will implement.
+* Concrete Creators: Implement the EmployeeCreator interface and define which concrete Employee type to create.
+* Concrete product: Implement the Employee interface, representing specific types of employee.
+ */
+
+// Abstract Creator
+type EmployeeCreator interface {
+	CreateEmployee() Employee
+}
+
+// Concreate Creator
+type DeveloperCreator struct {
+}
+
+func (c *DeveloperCreator) CreateEmployee(name string) Employee {
+	return &DeveloperEmployee{name}
+}
+
+func NewDeveloperCreator() *DeveloperCreator {
+	return &DeveloperCreator{}
+}
+
+type ManagerCreator struct {
+	name string
+}
+
+func (c *ManagerCreator) CreateEmployee(name string) Employee {
+	return &ManagerEmployee{name}
+}
+func NewManagerCreator() *ManagerCreator {
+	return &ManagerCreator{}
+}
+
+// Product interface
+type Employee interface {
+	GetName() string
+	SetName(name string)
+}
+
+// Product concrete
+type DeveloperEmployee struct {
+	name string
+}
+
+func (e *DeveloperEmployee) GetName() string {
+	return fmt.Sprintf("Developer name: %s", e.name)
+}
+func (e *DeveloperEmployee) SetName(name string) {
+	e.name = name
+}
+
+type ManagerEmployee struct {
+	name string
+}
+
+func (e *ManagerEmployee) GetName() string {
+	return fmt.Sprintf("Manager name: %s", e.name)
+}
+func (e *ManagerEmployee) SetName(name string) {
+	e.name = name
+}
+
+func main() {
+	devCreator := NewDeveloperCreator()
+	developer := devCreator.CreateEmployee("William")
+	fmt.Println(developer.GetName())
+
+	managerCreator := NewManagerCreator()
+	manager := managerCreator.CreateEmployee("Edward")
+	fmt.Println(manager.GetName())
+}
